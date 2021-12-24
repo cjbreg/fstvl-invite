bf5363dd91a920d27fd255c5ded0d5e7212a671e
Coen Breg <coenbreg@gmail.com>
2021-12-24 10:13:39 +0100

    updated video size

--- fstvlInvite/Views/ARCamera/ARCameraView.swift
+++ fstvlInvite/Views/ARCamera/ARCameraView.swift
@@ -74,8 +74,8 @@ struct ARViewContainer: UIViewRepresentable {
 
                     let anchorEntity = AnchorEntity(anchor: imageAnchor)
 
-                    let width = Float(imageAnchor.referenceImage.physicalSize.width * 1.01)
-                    let height = Float(imageAnchor.referenceImage.physicalSize.height * 1.01)
+                    let width = Float(imageAnchor.referenceImage.physicalSize.width * 1.05)
+                    let height = Float(imageAnchor.referenceImage.physicalSize.height * 1.05)
 
                     guard let videoUrl = URL(string: "https://i399015.hera.fhict.nl/invite-video.mp4") else {
                         fatalError("Video not found")
